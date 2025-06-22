850e6d02356a7e376d4e24947fcee57f03a94fc9
erfane99 <88964696+erfane99@users.noreply.github.com>
2025-06-22 12:10:34 +0300

    Updated auth-context.tsx

--- contexts/auth-context.tsx
+++ contexts/auth-context.tsx
@@ -841,6 +841,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 }
 
 export function useAuth() {
+  }
+  )
   const context = useContext(AuthContext);
   if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider');
